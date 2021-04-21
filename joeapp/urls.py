67186b114afc6e37cf916d868bbc6c5b27dcdc8a
Joseph Ebin <josefophe@gmail.com>
2021-04-21 19:39:54 +0100

    first commit

---
+++ joeapp/urls.py
@@ -0,0 +1,23 @@
+"""joeproject URL Configuration                                 
+The `urlpatterns` list routes URLs to views. For more informatio
+n please see:                                                       https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views                                                      1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name=
+'home')
+Including another URLconf
+    1. Import the include() function: from django.urls import in
+clude, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.ur
+ls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('index/',views.index,name = "index")
+]
